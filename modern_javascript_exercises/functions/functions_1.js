5046a563836d086c96f96158b02aa2efabd8b799
judejay <judejay26@gmail.com>
2023-10-17 14:12:32 +0100

    Intial Commit

--- modern_javascript_exercises/functions/functions_1.js
+++ modern_javascript_exercises/functions/functions_1.js
@@ -7,9 +7,15 @@
 
 const people = ["Harry", "Hermione", "Ron"];
 
+
 function logPerson(person) {
   console.log("The person is " + person);
 }
+
+
+
+
+
 
 people.forEach(logPerson);
 
@@ -17,14 +23,23 @@ people.forEach(logPerson);
 // Store the logPerson() function in a variable called performLogPerson
 
 // Your code here
-
+const performLogPerson = function logPerson(person) {
+  console.log("The person is " + person);
+}
 // Arrow Functions in a Variable
 // Write a version of logPerson() as an Arrow Function and store it in a variable called arrowVersionOfLogPerson
 
 // Your code here
-
+const logPerson = person =>{
+  console.log("The person is " + person);
+}
 // Anonymous Arrow Functions
 // Replace logPerson below with an Anonymous Arrow Function that does the same thing!
+
+
+person =>{
+  console.log("The person is " + person);
+}
 
 people.forEach(logPerson);
 
--- modern_javascript_exercises/rest_parameters/rest_parameters.js
+++ modern_javascript_exercises/rest_parameters/rest_parameters.js
@@ -1,9 +1,16 @@
 // Instructions can be found in rest_parameters.md
 
 // Add rest parameters here!
-export function add(a, b) {
+export function add(...rest) {
   // Add a loop here
-  return a + b;
-}
+ 
+    let total = 0;
+    for (const arg of rest) {
+      total += arg;
+    }
+    return total;
+  }
+  
+
 
 add(1, 2, 3, 4, 5);
